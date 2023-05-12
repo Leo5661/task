b7719f78f0b6c55d6933d35fbc13e666e7f3453c
Leo5661 <singhra66@gmail.com>
2023-05-12 23:57:57 +0530

    Added banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
+import ActiveButton from "@/components/ActiveButton";
 import Nav from "@/components/Nav";
+import style from "@/styles/page.module.scss";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <Nav />
+      <div className={style.company_banner}>
+        <div className={style.title_box}>
+          <div className={style.title}>WeWork + Salesforce</div>
+          <ActiveButton name={`Publish/Share`} />
+        </div>
+        <div className={style.banner}>
+          <div className={style.banner_img}>
+            <Image src="/banner.png" alt="banner" fill={true} />
+            <div className={style.cover}>
+              Workspace <br /> Proposal
+            </div>
+          </div>
+          <div className={style.dp}>
+            <Image src="/we.png" alt="we logo" width={60} height={60} />
+            <Image src="/sum.svg" alt="we logo" width={20} height={60} />
+            <Image src="/salesforce.png" alt="we logo" width={60} height={60} />
+          </div>
+        </div>
+        <div className="scope"></div>
+      </div>
     </main>
   );
 }
--- src/components/ActiveButton.tsx
+++ src/components/ActiveButton.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import style from "@/styles/button.module.scss";
 
-function ActiveButton() {
-  return <div>ActiveButton</div>;
+type Prop = {
+  name: String;
+};
+
+function ActiveButton(prop: Prop) {
+  return <div className={style.active_button}>{prop.name}</div>;
 }
 
 export default ActiveButton;
