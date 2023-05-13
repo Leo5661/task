4cb2efaf96f8437a1a67da9ebb605d81d25aea37
Leo5661 <singhra66@gmail.com>
2023-05-13 20:21:41 +0530

    layout fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,34 @@ export default function Home() {
   return (
     <main>
       <Nav />
-      <div className={style.company_banner}>
-        <div className={style.title_box}>
-          <div className={style.title}>WeWork + Salesforce</div>
-          <ActiveButton name={`Publish/Share`} />
-        </div>
-        <div className={style.banner}>
-          <div className={style.banner_img}>
-            <Image src="/banner.png" alt="banner" fill={true} />
-            <div className={style.cover}>
-              Workspace <br /> Proposal
+      <div className={style.body}>
+        <div className={style.company_banner}>
+          <div className={style.title_box}>
+            <div className={style.title}>WeWork + Salesforce</div>
+            <ActiveButton name={`Publish/Share`} />
+          </div>
+          <div className={style.banner}>
+            <div className={style.banner_img}>
+              <Image src="/banner.png" alt="banner" fill={true} />
+              <div className={style.cover}>
+                Workspace <br /> Proposal
+              </div>
+            </div>
+            <div className={style.dp}>
+              <Image src="/we.png" alt="we logo" width={60} height={60} />
+              <Image src="/sum.svg" alt="we logo" width={20} height={60} />
+              <Image
+                src="/salesforce.png"
+                alt="we logo"
+                width={60}
+                height={60}
+              />
             </div>
           </div>
-          <div className={style.dp}>
-            <Image src="/we.png" alt="we logo" width={60} height={60} />
-            <Image src="/sum.svg" alt="we logo" width={20} height={60} />
-            <Image src="/salesforce.png" alt="we logo" width={60} height={60} />
+          <div className={style.scope}>
+            <div className={style.scope_title}>Project Scope Recap</div>
+            <TabContainer />
           </div>
-        </div>
-        <div className={style.scope}>
-          <div className={style.scope_title}>Project Scope Recap</div>
-          <TabContainer />
         </div>
       </div>
     </main>
